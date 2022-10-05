8082ef9274f2dc11480697534bd3599a7363c74d
Phan Sang <phansanglittleemperor@gmail.com>
2022-10-05 10:43:51 +0700

    Update cuda_pathtracer.h

--- 1_GPU_Path_Tracing_Sang_Use_This_Version/cuda_pathtracer.h
+++ 1_GPU_Path_Tracing_Sang_Use_This_Version/cuda_pathtracer.h
@@ -1,24 +1,3 @@
-/*
-*  CUDA based triangle mesh path tracer using BVH acceleration by Sam lapere, 2016
-*  BVH implementation based on real-time CUDA ray tracer by Thanassis Tsiodras,
-*  http://users.softlab.ntua.gr/~ttsiod/cudarenderer-BVH.html
-*  Interactive camera with depth of field based on CUDA path tracer code
-*  by Peter Kutz and Yining Karl Li, https://github.com/peterkutz/GPUPathTracer
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 #ifndef __CUDA_PATHTRACER_H_
 #define __CUDA_PATHTRACER_H_
 
