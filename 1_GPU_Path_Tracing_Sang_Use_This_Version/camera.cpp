6f3a73a83d407c32a1e0e847b57960e1caa2f9cb
Phan Sang <phansanglittleemperor@gmail.com>
2022-10-05 10:44:32 +0700

    Update camera.cpp

--- 1_GPU_Path_Tracing_Sang_Use_This_Version/camera.cpp
+++ 1_GPU_Path_Tracing_Sang_Use_This_Version/camera.cpp
@@ -1,22 +1,3 @@
-/*
-*  CUDA based triangle mesh path tracer using BVH acceleration by Sam lapere, 2016
-*  Interactive camera with depth of field based on CUDA path tracer code
-*  by Peter Kutz and Yining Karl Li, https://github.com/peterkutz/GPUPathTracer
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 #include "camera.h"
 
 InteractiveCamera::InteractiveCamera()
