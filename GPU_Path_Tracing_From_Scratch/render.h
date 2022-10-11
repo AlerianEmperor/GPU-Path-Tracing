df83b4d88c36ac47c57eb9ba3ec14bd4fe3c31eb
Phan Sang <phansanglittleemperor@gmail.com>
2022-10-11 21:56:36 +0700

    Delete GPU_Path_Tracing_From_Scratch directory

--- GPU_Path_Tracing_From_Scratch/render.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RENDER_H_
-#define _RENDER_H_
-
-#include "vec3.h"
-
-void renderer(vec3* dptr, vec3* accumulatebuffer, unsigned framenumber, unsigned hashedframes);
-
-#endif // !_RENDER_H_
-
